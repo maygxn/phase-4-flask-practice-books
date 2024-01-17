9d0bbd4ab942b3ba30bd360e9a2ead5eabfbdc7c
Meagan Truong <MeaganTruong1@yahoo.com>
2024-01-17 10:31:47 -0600

    not checked

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask, make_response, jsonify, request
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
 
-from models import db # import your models here!
+from models import db, Author, Publisher, Book # import your models here!
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -22,6 +22,90 @@ def index():
     return "Hello world"
 
 # write your routes here!
+# route for author routes
+@app.route('/authors/<int:id>', methods=['GET', 'DELETE'])
+def author_by_id(id):
+    author = Author.query.filter(Author.id == id).first()
+
+    if request.method == 'GET':
+        try:
+            response = make_response(
+                author.to_dict(),
+                200
+            )
+        except:
+            response = make_response(
+                {"Error": "Author_ID does not exist"},
+                404
+            )
+    
+    elif request.method == 'DELETE':
+        try:
+            db.session.delete(author)
+            db.session.commit()
+            response = make_response(
+                {},
+                204
+            )
+        except:
+            response = make_response(
+                {'Error': "Author not found"},
+                404
+            )
+    return response
+
+@app.route('/books', methods=['GET', 'POST'])
+def get_books():
+    books = [book.to_dict() for book in Book.query.all()]
+
+    if request.method == 'GET':
+        response = make_response(
+            books,
+            200
+        )
+    
+    elif request.method ==  'POST':
+        try:
+            form_data = request.get_json()
+
+            new_book = Book(
+                title = form_data["title"],
+                page_count = form_data["page_count"],
+                author_id = form_data["author_id"],
+                publisher_id = form_data["publisher_id"]
+            )
+
+            db.session.add(new_book)
+            db.session.commit()
+
+            response = make_response(
+                new_book.to_dict(),
+                201
+            )
+        except ValueError:
+            response = make_response(
+                {"Errors": ["Validation errors"]},
+                400
+            )
+        
+    return response
+
+@app.route('/publishers/<int:id>', methods=['GET'])
+def publisher_by_id(id):
+    publisher = Publisher.query.filter(Publisher.id == id).first()
+
+    if request.method == 'GET':
+        try:
+            response = make_response(
+                publisher.to_dict(),
+                200
+            )
+        except:
+            response = make_response(
+                {"Error": "Publisher_ID does not exist"},
+                404
+            )
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,3 +11,50 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # write your models here!
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    page_count = db.Column(db.Integer, nullable=False)
+
+    # foreign keys
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    publisher_id = db.Column(db.Integer, db.ForeignKey('publishers.id'))
+
+    # relationships
+    authors = db.relationship('Author', back_populates = 'books')
+    publishers = db.relationship('Publisher', back_populates = 'books')
+
+    # serialization rules
+    # make tuple
+    serialize_rules = ('-authors.books', '-publishers.books')
+
+class Publisher(db.Model, SerializerMixin):
+    __tablename__ = 'publishers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    founding_year = db.Column(db.Integer, nullable=False)
+
+    # relationships
+    books = db.relationship('Book', back_populates = 'publishers')
+
+    # serialization rules
+    # make tuple
+    serialize_rules = ('-books.publishers', )
+
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    pen_name = db.Column(db.String)
+
+    # relationships
+    books = db.relationship('Book', back_populates = 'authors')
+
+    # serialization rules
+    # make tuple
+    serialize_rules = ('books.authors', )
